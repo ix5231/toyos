0622516f9bbc7d4099877786a0fb87926c0ca741
Ix5231 <ix5231if@gmail.com>
2018-02-25 10:04:24 +0900

    update

--- src/arch/x86_64/boot.asm
+++ src/arch/x86_64/boot.asm
@@ -3,7 +3,11 @@ global start
 section .text
 bits 32
 start:
+    ; initialize stack pointer
+    mov esp, stack_top
+
     ; print 'Hi from Ix OS'
+    ; green back, white character
     mov dword [0xb8000], 0x2f692f48 ; Hi
     mov dword [0xb8004], 0x2f662f20 ;  f
     mov dword [0xb8008], 0x2f6f2f72 ; ro
@@ -12,3 +16,16 @@ start:
     mov dword [0xb8014], 0x2f4f2f20 ;  O
     mov dword [0xb8018], 0x2f002f53 ; S
     hlt
+
+error
+    ; Prints 'ERR'
+    mov dword [0xb8000], 0x4f524f45
+    mov dword [0xb8004], 0x4f3a4f52
+    mov dword [0xb8008], 0x4f204f20
+    mov byte [0xb800a], al
+    hlt
+
+section .bss
+stack_bottom:
+    resb 64
+stack_top:
